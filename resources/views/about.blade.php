dd76686da82f8b874a64a6c17fd4a2c6d79720ea
Fahmida-Parvin <fahmidaparvin0@gmail.com>
2023-11-26 03:56:46 +0600

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,138 @@
+@extends('layouts.master')
+
+@section('content')
+<section id="about" class="about">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>About</h2>
+          <p>I'm a final year student of Computer Science and Engineering.Throughout my academic journey, I have gained a strong foundation in programming languages such as Java, C,React and Python, as well as web development technologies like HTML, CSS, and JavaScript.I have developed a Android Application which is an online Appointment system with my team also developed a E-commerce website.</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4" data-aos="fade-right">
+            <img src="images/portfolio.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+            <h3>Web Design &amp; Developer.</h3>
+            <p class="fst-italic">
+              Personal Details
+            </p>
+         <p>Hello, I'm Fahmida Parvin, a passionate and dedicated student based in Sylhet, Bangladesh. Currently pursuing my degree in Computer Science and Engineering (CSE) at  leading university, I am enthusiastic about exploring the dynamic world of technology. Throughout my academic journey, I have gained a solid foundation in programming languages such as Java, C, and Python. I have developed a strong understanding of data structures, algorithms, and software development principles. I enjoy solving complex problems and leveraging my skills to create innovative and efficient solutions.
+        If you're interested in collaboration or have any inquiries, feel free to explore my Fiverr profile at https://www.fiverr.com/fahmidaparvin05. I'm available for freelance opportunities and can be reached at fahmidaparvin0@gmail.com or by phone at +8801791702147. Looking forward to connecting with you!</p>
+
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+
+   <!-- ======= Skills Section ======= -->
+   <section id="skills" class="skills section-bg">
+    <div class="container">
+
+      <div class="section-title">
+        <h2>Skill</h2>
+        <p>As a beginner, i have gathered the following skills.Still now,I'm learning to improve skills.</p>
+      </div>
+
+      <div class="row skills-content">
+
+        <div class="col-lg-6" data-aos="fade-up">
+
+            <div class="progress">
+                <span class="skill">JavaScript <i class="val">75%</i></span>
+                <div class="progress-bar" role="progressbar" style="width: 75%;" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+
+            <div class="progress">
+                <span class="skill">PHP <i class="val">80%</i></span>
+                <div class="progress-bar" role="progressbar" style="width: 80%;" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+
+            <div class="progress">
+                <span class="skill">WordPress <i class="val">90%</i></span>
+                <div class="progress-bar" role="progressbar" style="width: 90%;" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+        </div>
+
+        <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+
+            <div class="progress">
+                <span class="skill">HTML <i class="val">100%</i></span>
+                <div class="progress-bar" role="progressbar" style="width: 100%;" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+
+
+              <div class="progress">
+                  <span class="skill">CSS <i class="val">90%</i></span>
+                  <div class="progress-bar" role="progressbar" style="width: 90%;" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+
+              <div class="progress">
+                  <span class="skill">Bootstrap <i class="val">80%</i></span>
+                  <div class="progress-bar" role="progressbar" style="width: 80%;" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+
+        </div>
+
+      </div>
+
+    </div>
+  </section><!-- End Skills Section -->
+
+    <section id="resume" class="resume">
+        <div class="container">
+
+          <div class="section-title">
+            <h2>Resume</h2>
+          </div>
+
+          <div class="row">
+            <div class="col-lg-6" data-aos="fade-up">
+
+              <div class="resume-item pb-0">
+                <h4>Fahmida Parvin </h4>
+                <p><em>I'm a student and a Beginner Web Developer.</em></p>
+                <ul>
+                  <li>Boroikhandi,Sunampur,Sylhet</li>
+
+                </ul>
+              </div>
+
+              <h3 class="resume-title">Education</h3>
+              <div class="resume-item">
+                <h4>B.Sc in CSE</h4>
+                <h5>2019 - 2023</h5>
+                <p><em>Leading University,Sylhet</em></p>
+              </div>
+              <div class="resume-item">
+                <h4>HSC</h4>
+                <h5>2016 - 2018</h5>
+                <p><em>Sylhet Science & Technology College <br>Mirer Moydan,Sylhet</em></p>
+
+
+            </div>
+            <div class="resume-item">
+              <h4>SSC</h4>
+              <h5>2014 - 2015</h5>
+              <p><em>Blue Bird School & College <br>Subid Bazar,Sylhet</em></p>
+            </div>
+          </div>
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+
+            <h3 class="resume-title">Working Experience</h3>
+            <div class="resume-item">
+              <h4>Support Engineer</h4>
+              <h5>ITOOLAPK</h5>
+              <ul>
+                <li>As a Support Engineer,Our primary focus is to provide a user-friendly interface that ensures a seamless experience for our visitors. We understand the importance of convenience and accessibility, and our platform is designed to cater to the needs and preferences of different users.With a diverse collection of APK files available on our platform, we aim to offer a wide range of choices to our users. Whether they are looking for popular applications or niche games, our goal is to meet their demands and provide them with the APKs they seek.<a href="https://itoolapk.com">Click here</a></li>
+              </ul>
+            </div>
+        </div>
+      </div>
+      </div>
+
+      </section><!-- End Resume Section -->
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.master')
+@section('content')
+<section class="section">
+    <div class="section__container">
+        <div class="content">
+            <p class="subtitle">HELLO</p>
+            <h1 class="title">
+                I'm <span>Fahmida Parvin<br />a</span> Web Developer
+            </h1>
+            <p class="description">
+                Hey there! Welcome to my portfolio website! I am Fahmida Parvin, a passionate and ambitious student pursuing a degree in Computer Science and Engineering. I am thrilled to share my journey, projects, and skills with you.
+            </p>
+            <div class="action__btns">
+
+                <button class="portfolio">Hire Me</button>
+            </div>
+        </div>
+        <div class="image">
+            <img src="images\portfolio.jpg" alt="profile" />
+        </div>
+    </div>
+</section>
+@endsection
